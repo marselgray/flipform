37783f91f54a9a09e619cba8d28c31145984064f
marselgray <marselgray@gmail.com>
2020-02-04 17:25:21 -0500

    Update: form validation begin testing

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 
+// flipping
 const btn = document.querySelector('.front--btn');
 const flip = document.querySelector('.cont--flip');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () => {
 	flip.classList.add('flipped');
-})
+});
+
+
+// validation
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+function checkInputs(){
+	console.log('i was checked')
+}
