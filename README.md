484c0e45782892750a9a30764e12533e1eb1ec79
marselgray <marselgray@gmail.com>
2020-02-06 11:37:56 -0500

    Update: form validation functions

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ a form validation example
 [HTML5 input types](https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types)
 [CSS Tricks Form Validation](https://css-tricks.com/form-validation-part-1-constraint-validation-html/)
 
+https://codepen.io/devdanny/pen/JgLjk
+
+https://www.happyhues.co/palettes/2
+
 ## Planning
 1. Validations
 2. Front of card
--- app.js
+++ app.js
@@ -13,27 +13,79 @@ const form = document.querySelector('.form');
 const input = document.getElementsByClassName('form--box__required');
 const badges = document.getElementsByClassName('form--box__badge');
 
+
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	checkInputs();
-
 });
 
 function checkInputs(){
-	console.log('check inputs');
 	checkFirstName();
+	checkLastName();
+	checkPhone();
+	checkEmail();
+}
+
+// use to validate names / text options
+function alphanumeric(data) { 
+	let letters = /^[0-9a-zA-Z]+$/;
+	if (letters.test(data)) {
+		return true;
+	} else{
+		return false;
+	}
 }
 
 function checkFirstName(){
 	let first = document.getElementById('firstname');
-	console.log(first.value)
 	if (first.value === ''){
-		console.log('empty')
-
+		error(first, 'Enter a First Name');
 	} else{
-		console.log('not empty')
+		alphanumeric(first.value);
+	}
+}
 
+function checkLastName(){
+	let last = document.getElementById('lastname');
+	if (last.value === ''){
+		error(last, 'Enter a Last Name');
+	} else{
+		alphanumeric(last.value);
+	}
+}
+
+function checkPhone(){
+	let tel = document.getElementById('tel');
+	let testData = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+	if (tel.value === ''){
+		error(tel, 'Enter a Phone Number');
+	} else{
+		if(testData.test(tel.value)) {
+			return true;
+		} else{
+			return false;
+		}
+	}
+}
+
+function checkEmail(){
+	let email = document.getElementById('email');
+	let testData = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (email.value === ''){
+		error(email, 'Enter an Email');
+	} else{
+		if(testData.test(email.value)) {
+			return true;
+		} else{
+			return false;
+		}
 	}
 }
 
 
+
+// returns the error for that element and changes the placeholder text
+function error(element, text){
+	element.nextElementSibling.style.display = 'block';
+	element.placeholder = text;
+}
